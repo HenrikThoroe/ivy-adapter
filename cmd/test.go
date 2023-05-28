32d8da00295019ed76f688ea1b0bfa1bd548343a
Henrik Thoroe <henrik@thoroee.com>
2023-05-28 18:03:28 +0200

    feat(test): add test command

---
+++ cmd/test.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/HenrikThoroe/ivy-adapter/internal/app/test"
+	"github.com/HenrikThoroe/ivy-adapter/internal/pkg/conf"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
+
+var testCmd = &cobra.Command{
+	Use:   "test",
+	Short: "Run headless tests and sync with server",
+	Long: "The test command connects to the test server based on the configuration file.\n" +
+		"The programm will report the system stats to the test server and wait until the server requests a test to run.\n" +
+		"A test will download the requested engines and play a batch of games.\n" +
+		"The number of games played depends on the number of cores and memory available on the system.\n" +
+		"Use q or ctrl+c to exit at any time.",
+	Run: func(cmd *cobra.Command, args []string) {
+		model := test.BuildTestViewModel()
+
+		if _, err := tea.NewProgram(model).Run(); err != nil {
+			fmt.Println("Error running program: ", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(testCmd)
+	conf.Load()
+}
