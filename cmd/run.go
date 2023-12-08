308c4fa77cced0952bb10901e74d3626037a7ea7
Henrik Thoroe <henrik@thoroee.com>
2023-12-08 12:01:42 +0100

    feat(run): use updated API

--- cmd/run.go
+++ cmd/run.go
@@ -6,14 +6,12 @@ import (
 
 	"github.com/HenrikThoroe/ivy-adapter/internal/app/run"
 	"github.com/HenrikThoroe/ivy-adapter/internal/pkg/conf"
-	"github.com/HenrikThoroe/ivy-adapter/internal/pkg/mgmt"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
 type _runFlags struct {
-	game    string
-	color   string
+	player  string
+	exe     string
 	engine  string
 	version string
 }
@@ -23,47 +21,43 @@ var runFlags _runFlags
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Runs the engine on the given game ID",
-	Long: "Launches the engine given by name and version.\n" +
-		"Will fail if the engine has not been previously installed.\n" +
-		"The game is played by connecting against the game server which has to be available\n" +
-		"on the configured address in the config file.\n" +
-		"Use q or ctrl+c to exit at any time.",
+	Long: "Launches the engine given by name and version or path.\n" +
+		"A connection against the game host server will be created using the given player ID.\n" +
+		"Once the game server sends a move request, the engine will be invoked to calculate a move.\n" +
+		"The output from the engine and all prompts to the engine will be printed to the console.\n",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		version, vErr := mgmt.ParseVersion(runFlags.version, mgmt.DotVersionStyle)
+		conf.Load()
 
-		if vErr != nil {
-			fmt.Println("Error parsing version: ", vErr)
+		ifc, stdin, stdout, err := run.SetupEngineIfc(runFlags.exe, runFlags.engine, runFlags.version)
+
+		if err != nil {
+			fmt.Println("Error setting up engine interface: ", err)
 			os.Exit(1)
 		}
 
-		eng, eErr := mgmt.BestMatch(runFlags.engine, *version)
-
-		if eErr != nil {
-			fmt.Println("Error parsing engine: ", eErr)
-			os.Exit(1)
+		pipe := func(c chan string, prefix string) {
+			for s := range c {
+				fmt.Println(prefix + s)
+			}
 		}
 
-		vm := run.BuildRunViewModel(runFlags.game, runFlags.color, eng)
+		go pipe(stdin, "> ")
+		go pipe(stdout, "")
 
-		if _, err := tea.NewProgram(vm).Run(); err != nil {
-			fmt.Println("Error running program: ", err)
-			os.Exit(1)
-		}
+		run.Play(ifc, runFlags.player)
+		os.Exit(0)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-	runCmd.Flags().StringVarP(&runFlags.game, "game", "g", "", "Game ID")
-	runCmd.Flags().StringVarP(&runFlags.color, "color", "c", "", "Color the selected engine should play as")
+	runCmd.Flags().StringVarP(&runFlags.player, "player", "p", "", "Player ID")
+	runCmd.Flags().StringVarP(&runFlags.exe, "binary", "b", "", "Path to engine executable")
 	runCmd.Flags().StringVarP(&runFlags.engine, "engine", "e", "", "Engine Name (must be installed)")
 	runCmd.Flags().StringVarP(&runFlags.version, "version", "v", "", "Version of Engine (must be installed)")
 
-	runCmd.MarkFlagRequired("game")
-	runCmd.MarkFlagRequired("color")
-	runCmd.MarkFlagRequired("engine")
-	runCmd.MarkFlagRequired("version")
+	runCmd.MarkFlagRequired("player")
 
 	conf.Load()
 }
